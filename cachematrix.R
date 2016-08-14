4e4179e08f37d1fa3f851eb8ee4e37b5bd99d61b
JESUS SUNIAGA <suniaga@users.noreply.github.com>
2016-08-14 09:53:19 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,4 +1,5 @@
--## Put comments here that give an overall description of what your
+-## Instructions:
+## Put comments here that give an overall description of what your
 -## functions do
 +## cachematrix.R
 +##
